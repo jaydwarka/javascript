c205b8127f631790048b6676b17567d95e6d1c88
jay <2138668@talnet.nl>
2021-05-10 16:15:27 +0200

    added stuff

---
+++ h2/h2.2.js
@@ -0,0 +1,4 @@
+document.getElementById("bl1").style.backgroundColor = "Aqua";
+document.getElementById("bl2").style.backgroundColor = "#006600";
+document.getElementById("bl3").style.backgroundColor = "";
+document.getElementById("bl4").style.backgroundColor = "red";
--- h2/h2.js
+++ h2/h2.js
@@ -1,2 +1,4 @@
-document.getElementById("paginatitel").innerText = "mijn geweldige pagina"
-document.getElementById("subkop").innerText = "mijn geweldige subkop"
+document.getElementById("paginatitel").innerText = "mijn geweldige pagina";
+document.getElementById("subkop").innerText = "mijn geweldige subkop";
+document.getElementById("aap1").src = "img/boerenkool.jpg";
+document.getElementById("aap2").src = "img/kip.jpg";
